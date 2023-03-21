5479f2a4851a6d2fc65dc5e0b5e2492ca56d175f
Dy Tran <duygtran1706@gmail.com>
2023-03-21 23:06:45 +0700

    add navigate screens

--- app/src/main/java/com/example/shoppeeandroid/MainActivity.java
+++ app/src/main/java/com/example/shoppeeandroid/MainActivity.java
@@ -2,16 +2,36 @@ package com.example.shoppeeandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView btnBuyCart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar().hide();
-        setContentView(R.layout.payment_main);
+        setContentView(R.layout.home_main);
+        getViewActivityCart();
+        buyCart();
     }
+    private void getViewActivityCart(){
+        btnBuyCart = (TextView) findViewById(R.id.btnBuyCart);
+    }
+    private void buyCart(){
+        btnBuyCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Payment.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
 }
---
+++ app/src/main/java/com/example/shoppeeandroid/Payment.java
@@ -0,0 +1,47 @@
+package com.example.shoppeeandroid;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Payment extends AppCompatActivity {
+    private TextView payment;
+    private ImageView navigate;
+    @Override
+    protected void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        setContentView(R.layout.payment_main);
+        getViewScreenPayment();
+        confirmPayment();
+        navigateToCart();
+    }
+    private void getViewScreenPayment(){
+        payment=(TextView) findViewById(R.id.btnPayment);
+        navigate = (ImageView) findViewById(R.id.navigatePayment);
+    }
+    private void confirmPayment(){
+        payment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("Thanh toán thành công!");
+            }
+        });
+    }
+    private void navigateToCart(){
+        navigate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Payment.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
